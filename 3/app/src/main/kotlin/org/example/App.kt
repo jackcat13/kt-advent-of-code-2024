a0e4f11a7c58d0d6707b77aacfd4ce3cc8cf837d
jackcat13 <christophehenry11@gmail.com>
2024-12-03 15:33:47 +0100

    feat: day 3 completed

--- 3/app/src/main/kotlin/org/example/App.kt
+++ 3/app/src/main/kotlin/org/example/App.kt
@@ -4,9 +4,28 @@
 package org.example
 
 import java.io.File
+import java.nio.CharBuffer
+import kotlin.collections.first
 
 fun main() {
-    val file = File("src/main/resources/list.txt")
-    val list = file.readLines().map { 
-    }
+    val file = File("src/main/resources/listExo.txt")
+    val list = file.readLines().toString().withoutDisabled().toUncorrupted()
+    
+    val result = list.sum()
+    
+    println(result)
+}
+
+fun String.toUncorrupted(): List<Int> {
+    val regex = Regex("""mul\([0-9]{1,3},[0-9]{1,3}\)""")
+    return regex.findAll(this).map {
+        it.groupValues.map {
+            it.substringAfter("mul(").substringBefore(",").toInt() * it.substringAfter(",").substringBefore(")").toInt()
+        }.sum()
+    }.toList()
+}
+
+fun String.withoutDisabled(): String {
+    val regex = Regex("""don\'t\(\).*?do\(\)""")
+    return regex.replace(this) { "" }
 }
